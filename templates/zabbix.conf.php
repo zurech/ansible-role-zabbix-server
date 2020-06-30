475e321d64b44377d66a08f0ff7b2b0c87219b12
Rodrigo Echaide <rodrigoechaide@gmail.com>
2020-06-30 08:44:37 +0200

    Partial support for Ubuntu Bionic and Raspbian

--- templates/zabbix.conf.php
+++ templates/zabbix.conf.php
@@ -2,18 +2,18 @@
 // Zabbix GUI configuration file.
 global $DB;
 
-$DB['TYPE']     = '{{db_engine}}';
-$DB['SERVER']   = '{{ZBX_DB_IP}}';
+$DB['TYPE']     = '{{ db_engine|upper }}';
+$DB['SERVER']   = 'localhost';
 $DB['PORT']     = '0';
-$DB['DATABASE'] = '{{db_name}}';
-$DB['USER']     = '{{db_user}}';
-$DB['PASSWORD'] = '{{db_password}}';
+$DB['DATABASE'] = '{{ db_name }}';
+$DB['USER']     = '{{ db_user }}';
+$DB['PASSWORD'] = '{{ db_password }}';
 
 // Schema name. Used for IBM DB2 and PostgreSQL.
 $DB['SCHEMA'] = '';
 
-$ZBX_SERVER      = '{{ZBX_SERVER_IP}}';
+$ZBX_SERVER      = 'localhost';
 $ZBX_SERVER_PORT = '10051';
-$ZBX_SERVER_NAME = '{{zabbix_server_name}}';
+$ZBX_SERVER_NAME = '{{ zabbix_server_name }}';
 
 $IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
